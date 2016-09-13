39dadd75796de394dfc38777a0ebd9f4e99ecbf5
Atikom <atikom.sn@gmail.com>
2016-09-13 14:41:20 +0700

    First Commit Testing V.1.01 - Camera Capture - Barcode Reader - GPS Location (Latitude and Longtitude)

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,349 @@
+package com.example.administrator.myapplication;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import android.content.pm.PackageManager;
+//import android.location.Location;
+
+//import android.location.LocationListener;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private static final int CONTENT_REQUEST = 1337;
+
+    public GoogleApiClient googleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        Button captureBtn = (Button) findViewById(R.id.capture);
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int REQUEST_CAMERA = 0;
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+                String imageFileName = "IMG_" + timeStamp + ".jpg";
+
+                File f = new File(Environment.getExternalStorageDirectory()
+                        , "DCIM/Camera/" + imageFileName);
+
+                Uri uri = Uri.fromFile(f);
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+
+                startActivityForResult(Intent.createChooser(intent
+                        , "Take a picture with"), REQUEST_CAMERA);
+            }
+        });
+
+        Button scanBtn = (Button) findViewById(R.id.scan);
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator scanIntegrator = new IntentIntegrator(MainActivity.this);
+                scanIntegrator.initiateScan();
+            }
+        });
+
+//        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+//        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+//        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        MyCurrentLoctionListener locationListener = new MyCurrentLoctionListener();
+
+        // Here, thisActivity is the current activity
+
+
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        //Log.v("xxxxxx", LocationManager.GPS_PROVIDER.toString());
+
+        //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (googleApiClient != null && googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    /*@Override
+    public void onLocationChanged(Location location) {
+        TextView txtLat = (TextView) findViewById(R.id.textView);
+        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+    }*/
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        System.gc();
+
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (scanningResult != null) {
+            //we have a result
+            Log.v("xxxxxx",  " |NNNN.msn");
+
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+
+            TextView textView = (TextView) findViewById(R.id.textView);
+            //String contents = data.getStringExtra("SCAN_RESULT");
+            textView.setText(scanContent+" "+scanFormat);
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    scanContent+" "+scanFormat, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CONTENT_REQUEST) {
+
+                /*Intent i=new Intent(Intent.ACTION_VIEW);
+
+                i.setDataAndType(Uri.fromFile(output), "image/jpeg");
+                startActivity(i);
+                finish();*/
+
+                // Save a file: path for use with ACTION_VIEW intents
+                //mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+
+                /*Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                File f = new File(mCurrentPhotoPath);
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);*/
+
+
+
+                Log.v("xxxxxx",  " |NNNN.msn");
+                //data.putExtra(MediaStore.EXTRA_OUTPUT, "android.jpg");
+                //use imageUri here to access the image
+
+                //Bundle extras = data.getExtras();
+
+                //Log.e("URI",imageUri.toString());
+
+                //Bitmap bmp = (Bitmap) extras.get("data");
+
+                // here you will get the image as bitmap
+
+                /*File outFile = new File(Environment.getExternalStorageDirectory(), "myname.jpeg");
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(outFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                photo.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                fos.flush();
+                fos.close();*/
+
+
+            }
+            else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Picture was not taken", Toast.LENGTH_SHORT);
+            } else if (resultCode == 2)
+            {
+                Log.v("xxxxxx", "tttttttttt");
+
+                //TextView textView = (TextView) findViewById(R.id.textView);
+                String contents = data.getStringExtra("SCAN_RESULT");
+                //textView.setText(contents);
+                /*if (resultCode == RESULT_OK)
+                {
+                    TextView textView = (TextView) findViewById(R.id.textView);
+                    String contents = data.getStringExtra("SCAN_RESULT");
+                    String format = data.getStringExtra("SCAN_RESULT_FORMAT");
+                    textView.setText("Result : " + contents);
+                }*/
+            }
+        }
+
+
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        LocationAvailability locationAvailability = LocationServices.FusedLocationApi.getLocationAvailability(googleApiClient);
+        if(locationAvailability.isLocationAvailable()) {
+            LocationRequest locationRequest = new LocationRequest()
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                    .setInterval(5000);
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest,  this);
+        } else {
+            Log.v("xxxxxx","qwrqwrwerwer");
+            // Do something when location provider not available
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Latitude : " + location.getLatitude() + "\n" +
+                "Longitudesm : " + location.getLongitude());
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
